3f5936a8832770a99424cebdec8c9aab308d1d95
ngomanhthong <ngomanhthong0812@gmail.com>
2023-06-17 07:59:48 +0700

    Project1

---
+++ style.js
@@ -0,0 +1,14 @@
+
+var menuTitleDad = document.querySelector(".title-dad .menu")
+
+back.addEventListener('click', function(){
+    tab.classList.add('hidden-back')
+})
+next.addEventListener('click', function(){
+    tab.classList.add('hidden-next')
+})  
+function anMenu(){
+    menuTitleDad.classList.toggle('menu-dad')
+} 
+     
+
