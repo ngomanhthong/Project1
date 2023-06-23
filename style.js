5d92f796355fd0d73399ee9a5b6b8f6c040e0d07
ngomanhthong <ngomanhthong0812@gmail.com>
2023-06-23 18:22:34 +0700

    Project1

--- style.js
+++ style.js
@@ -1,15 +1,20 @@
-
 var menuTitleDad = document.querySelector(".title-dad .menu")
-
-back.addEventListener('click', function(){
-    tab.classList.add('hidden-back')
-})
-next.addEventListener('click', function(){
-    tab.classList.add('hidden-next')
-})  
 function anMenu(){
-    menuTitleDad.classList.toggle('menu-dad')
+    console.log(menuTitleDad.classList.toggle('menu-dad'))
 } 
+
+var menu = document.querySelector(".container-reponsive .bx-menu")
+var containerLeft = document.querySelector(".container-left")
+var containerLeftFather = document.querySelector(".container-left-father")
+  menu.addEventListener("click", function(){
+    containerLeftFather.classList.add("on")
+    containerLeft.classList.add("on")
+  })
+  containerLeftFather.addEventListener("click",function(){
+    containerLeftFather.classList.remove("on")
+    containerLeft.classList.remove("on")
+  })
+
 
 function san_pham(index) {
     window.location.href = "San_pham_" + index + ".html";
